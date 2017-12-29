d47a9aacec4ebeb36539c8abe07ea0daaf349c85
Lukasz Janyst <lukasz@jany.st>
2017-12-29 04:05:47 +0100

    Rename the app and remove the react template code

---
+++ src/components/ReadableApp.js
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------------
+// Author: Lukasz Janyst <lukasz@jany.st>
+// Date: 29.12.2017
+//------------------------------------------------------------------------------
+
+import React, { Component } from 'react';
+
+class ReadableApp extends Component {
+  render() {
+    return (
+      <div>
+        Hello, world!
+      </div>
+    );
+  }
+}
+
+export default ReadableApp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ReadableApp from './components/ReadableApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ReadableApp />, document.getElementById('root'));
 registerServiceWorker();
