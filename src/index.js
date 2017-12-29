52c8f894432b3d18cb460b2fcffd044d9b02901d
Lukasz Janyst <lukasz@jany.st>
2017-12-29 07:51:53 +0100

    Include bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import ReadableApp from './components/ReadableApp';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
