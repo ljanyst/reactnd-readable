f8e3a254843d711c9ac3c3e3ac6adcaf4c56efc4
Lukasz Janyst <lukasz@jany.st>
2017-12-29 20:29:55 +0100

    Add comments to the API

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,10 +3,10 @@
 // Date: 29.12.2017
 //------------------------------------------------------------------------------
 
+//------------------------------------------------------------------------------
+// Set up the API
+//------------------------------------------------------------------------------
 const api = "http://localhost:3001";
-
-
-// Generate a unique token for storing your bookshelf data on the backend server.
 let token = localStorage.token;
 if (!token)
   token = localStorage.token = Math.random().toString(36).substr(-8);
@@ -16,8 +16,10 @@ const headers = {
   'Authorization': token
 };
 
+//------------------------------------------------------------------------------
+// Get categories
+//------------------------------------------------------------------------------
 export const getCategories = () =>
   fetch(`${api}/categories`, { headers })
   .then(res => res.json())
   .then(data => data.categories);
-
