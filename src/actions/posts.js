7b8f597c57fa9fd2d5d758cbed947dcd3c7d8919
Lukasz Janyst <lukasz@jany.st>
2017-12-30 21:47:42 +0100

    Implement POST_UP_VOTE, POST_DOWN_VOTE, and POST_DELETE redux actions

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -3,11 +3,35 @@
 // Date: 29.12.2017
 //------------------------------------------------------------------------------
 
-export const SET_POSTS = 'SET_POSTS';
+export const POSTS_SET = 'POSTS_SET';
+export const POST_UP_VOTE = 'POST_UP_VOTE';
+export const POST_DOWN_VOTE = 'POST_DOWN_VOTE';
+export const POST_DELETE = 'POST_DELETE';
 
 export function setPosts(postList) {
   return {
-    type: SET_POSTS,
+    type: POSTS_SET,
     postList
+  };
+}
+
+export function postUpVote(id) {
+  return {
+    type: POST_UP_VOTE,
+    id
+  };
+}
+
+export function postDownVote(id) {
+  return {
+    type: POST_DOWN_VOTE,
+    id
+  };
+}
+
+export function postDelete(id) {
+  return {
+    type: POST_DELETE,
+    id
   };
 }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -3,13 +3,25 @@
 // Date: 29.12.2017
 //------------------------------------------------------------------------------
 
-import { SET_POSTS } from '../actions/posts';
+import {
+  POSTS_SET, POST_UP_VOTE, POST_DOWN_VOTE, POST_DELETE
+} from '../actions/posts';
+import { cfm } from '../utils/helpers';
 
 export function postReducer(state = [], action) {
   switch(action.type) {
 
-  case SET_POSTS:
+  case POSTS_SET:
     return action.postList;
+
+  case POST_UP_VOTE:
+    return cfm(state, action.id, (post) => { post.voteScore++; });
+
+  case POST_DOWN_VOTE:
+    return cfm(state, action.id, (post) => { post.voteScore--; });
+
+  case POST_DELETE:
+    return state.filter(post => post.id !== action.id);
 
   default:
     return state;
