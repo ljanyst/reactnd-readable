00caaa6b2aa69d28c7a8d6cc0dadded37e7f5558
Lukasz Janyst <lukasz@jany.st>
2017-12-31 11:37:18 +0100

    Remove the cfm helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12,16 +12,3 @@ export function categoryExists(categoryPath, categories) {
   const match = findBy.call(categories, 'path', categoryPath);
   return match[1] !== -1 ? true : false;
 }
-
-//------------------------------------------------------------------------------
-// Copy, find, and modify
-//------------------------------------------------------------------------------
-export function cfm(list, id, modder) {
-  var newList = list.slice();
-  const match = findBy.call(list, 'id', id);
-  if(match[1] === -1)
-    return newList;
-
-  modder(newList[match[1]]);
-  return newList;
-}
