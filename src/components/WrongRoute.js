5e63c4e651fe178caa512ac41333dcb8538f1fae
Lukasz Janyst <lukasz@jany.st>
2017-12-31 12:58:23 +0100

    Refactor WrongRoute to a functional component

--- src/components/WrongRoute.js
+++ src/components/WrongRoute.js
@@ -3,27 +3,16 @@
 // Date: 29.12.2017
 //------------------------------------------------------------------------------
 
-import React, { Component } from 'react';
+import React from 'react';
 import DirectionsIcon from 'react-icons/lib/md/directions';
 
-//------------------------------------------------------------------------------
-// Wrong route
-//------------------------------------------------------------------------------
-class WrongRoute extends Component {
-
-  //----------------------------------------------------------------------------
-  // Render the component
-  //----------------------------------------------------------------------------
-  render() {
-    return (
-      <div className='col-md-8 col-md-offset-2'>
-        <div align='center'>
-          <DirectionsIcon size={300} color='DimGrey'/>
-          <h3>It is not what you're looking for.</h3>
+export default function WrongRoute(props) {
+  return (
+    <div className='col-md-8 col-md-offset-2'>
+      <div align='center'>
+        <DirectionsIcon size={300} color='DimGrey'/>
+        <h3>It is not what you're looking for.</h3>
         </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default WrongRoute;
