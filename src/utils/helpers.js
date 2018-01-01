af6f2aacde14829489d1367bc5c267a68c403db6
Lukasz Janyst <lukasz@jany.st>
2018-01-01 14:57:49 +0100

    Implement a helper for creating random Ids

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12,3 +12,16 @@ export function categoryExists(categoryPath, categories) {
   const match = findBy.call(categories, 'path', categoryPath);
   return match[1] !== -1 ? true : false;
 }
+
+//------------------------------------------------------------------------------
+// Make id
+//------------------------------------------------------------------------------
+export function makeId(length) {
+  var text = '';
+  var possible = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+  for(var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
